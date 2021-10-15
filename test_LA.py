348b00c54a3bd99442d8728f9a132f68cccbc4f0
edpallar <90791746+edpallar@users.noreply.github.com>
2021-10-15 11:28:21 -0500

    Unit Testing the Linear Algebra functions

---
+++ test_LA.py
@@ -0,0 +1,57 @@
+import LA 
+pytest.command
+
+
+
+
+test_vector_01 = [1, 2, 4]
+test_vector_02 = [3, 1, 2]
+test_vector_03 = [3, 6, 9]
+
+test_matrix_01 = [[1,2,3],[4,5,6],[7,8,9]]
+test_matrix_02 = [[8,3,4],[6,15,7],[0,72,1]] 
+test_matrix_03 = [[1,1,1],[2,2,2],[3,3,3]]
+
+# Problem #0
+
+def test_add_vectors(): 
+    # add_vectors(test_vector_01,test_vector_02) should output [4,3,6]
+    assert LA.add_vectors(test_vector_01,test_vector_02) == [4,3,6]
+    # add_vectors(test_vector_01, test_vector_03) should ouput [4,8,13]
+    assert LA.add_vectors(test_vector_01,test_vector_03) == [4,8,13]
+
+#Problem 01
+
+def test_vector_scalar_multi():
+    assert 
+  
+
+
+
+#Problem 02
+
+def test_matrix_scalar_multi():
+    assert
+  
+
+
+#Problem 03
+
+def test_add_matrices():
+    assert
+  
+
+
+#Problem 04
+
+def test_matrix_vector_multi():
+    assert 
+
+  
+
+
+#Problem 05
+
+def test_matrix_matrix_multi():
+    assert
+  
