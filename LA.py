07ec812195fc9dab302a469244b45fb8208196cd
edpallar <90791746+edpallar@users.noreply.github.com>
2021-10-15 23:23:51 -0500

    Add files via upload

--- LA.py
+++ LA.py
@@ -1,34 +1,5 @@
-"""
-This homework is due on 10/15/2021 by 11:59pm. 
+# Problem 0
 
-
-For this assignment you will be writing a python script to be named LA.py. In
-this script you will need to write 6 functions. Every function must 
-
-1) Have a doc string.
-
-2) Have type annotations
-
-3) Be tested using unit testing. 
-
-Once you have finished writing LA.py you will upload it to the same github repo
-you used for HW02. The functions you need to write are  
-"""
-#Test Inputs
-
-
-
-test_vector_01 = [1, 2, 4]
-test_vector_02 = [3, 1, 2]
-test_vector_03 = [3, 6, 9]
-
-test_matrix_01 = [[1,2,3],[4,5,6],[7,8,9]]
-test_matrix_02 = [[8,3,4],[6,15,7],[0,72,1]] 
-test_matrix_03 = [[1,1,1],[2,2,2],[3,3,3]]
-
-#Example:
-
-# Problem #0
 
 def add_vectors(vector_a: list, vector_b: list) -> list:
     """Adds the two input vectors.
@@ -49,12 +20,6 @@ def add_vectors(vector_a: list, vector_b: list) -> list:
     for index in range(len(result)):
         result[index] = vector_a[index] + vector_b[index]
     return result
-
-# End Example
-# Note that you must add unit tests for problem 0!!!!!
-# vector_scalar_multli(test_vector_01,2) should output [2,4,8]
-print('Problem0:Test Output for add_vectors: ' + str(add_vectors(test_vector_01,test_vector_02)))
-print('Should have been [4,3,6]')
 
 
 #Problem 01
@@ -79,19 +44,11 @@ def vector_scalar_multi(vector: list, scalar: float):
     result.append(vector[index] * scalar)
   return result
 
-# vector_scalar_multli(test_vector_01,2) should output [2,4,8]
-print('Problem1:Test Output for vector_scalar_multi: ' + str(vector_scalar_multi(test_vector_01,2)))
-print('Should have been [2,4,8]')
-
-
-# vector_scalar_multli(test_vector_03,2) should output [6,12,18]
-print('Problem1:Test Output for vector_scalar_multi: ' + str(vector_scalar_multi(test_vector_03,2)))
-print('Should have been [6, 12, 18]')
 
 
 #Problem 02
 
-def matrix_scalar_multi(matrix: list, scalar: "float"):
+def matrix_scalar_multi(matrix: list, scalar: float):
   """Multiplies a matrix by a scalar.
 
     Creates a result matrix as an empty set. It then goes through each element 
@@ -107,22 +64,15 @@ def matrix_scalar_multi(matrix: list, scalar: "float"):
     Returns:
        The multiplication of the input matrix and scalar stored as a new matrix as result. 
     """
-  result: "list[list[float]]"= []
+  result = []
   for index in range((len(matrix))):
     result.append(vector_scalar_multi(matrix[index], scalar))
   return result 
 
-# matrix_scalar_multi(test_matrix_01,2) should output [[2,4,6],[8,10,12],[14,16,18]]
-print('Problem2:Test Output for matrix_scalar_multi: ' + str(matrix_scalar_multi(test_matrix_01,2)))
-print('Should have been [[2,4,6],[8,10,12],[14,16,18]]')
-
-# matrix_scalar_multi(test_matrix_01,3) should output [[3,6,9],[12,15,18],[21,24,27]]
-print('Problem2:Test Output for matrix_scalar_multi: ' + str(matrix_scalar_multi(test_matrix_01,3)))
-print('Should have been [[3,6,9],[12,15,18],[21,24,27]]')
 
 #Problem 03
 
-def add_matrices(matrix_a: "list[list[float]]", matrix_b: "list[list[float]]"):
+def add_matrices(matrix_a: list, matrix_b: list):
   """Adds the two input matrices.
 
     Creates a result matrix stored as an empty list. Using our function #0, we 
@@ -137,22 +87,15 @@ def add_matrices(matrix_a: "list[list[float]]", matrix_b: "list[list[float]]"):
     Returns:
        The sum of the input matrices stored as a new result list. 
     """
-  result: "list[list[float]]"= []
+  result = []
   for index in range(len(matrix_a)):
     result.append(add_vectors(matrix_a[index],matrix_b[index]))
   return result
 
-# add_matrices(test_matrix_01,test_matrix_02) should output [[9,5,7],[10,20,13],[7,80,10]]
-print('Problem 3:Test Output for add_matrices: ' + str(add_matrices(test_matrix_01,test_matrix_02)))
-print('Should have been [[9,5,7],[10,20,13],[7,80,10]]')
-
-# add_matrices(test_matrix_01,test_matrix_03) should output [[2,3,4],[6,7,8],[10,11,12]]
-print('Problem 3:Test Output for add_matrices: ' + str(add_matrices(test_matrix_01,test_matrix_03)))
-print('Should have been [[2,3,4],[6,7,8],[10,11,12]]')
 
 #Problem 04
 
-def matrix_vector_multi(matrix: "list[list[float]]", vector: "list[float]"):
+def matrix_vector_multi(matrix: list, vector: list):
   """Multiplies a matrix by a vector.
 
     The number of columns of matrix must equal the number of components in our 
@@ -173,8 +116,8 @@ def matrix_vector_multi(matrix: "list[list[float]]", vector: "list[float]"):
        The multiplication of the input matrix and a vector stored as a new 
        matrix as result. 
     """
-  matrix_r: "list[list[float]]" = [0 for element in matrix]
-  result: "list[float]"= [0]
+  matrix_r = [0 for element in matrix]
+  result = [0]
   for index in range(len(vector)):
     matrix_r[index] = vector_scalar_multi(matrix[index],vector[index])
   result = add_vectors(matrix_r[0],matrix_r[1])
@@ -182,17 +125,10 @@ def matrix_vector_multi(matrix: "list[list[float]]", vector: "list[float]"):
     result = add_vectors(result, matrix_r[index])
   return result 
 
-# matrix_vector_multi(test_matrix_01,test_vector_1) should output [37,44,51]
-print('Problem 4:Test Output for matrix_vector_multi: ' + str(matrix_vector_multi(test_matrix_01,test_vector_01)))
-print('Should have been [37,44,51]')
-
-# matrix_vector_multi(test_matrix_01,test_vector_2) should output [21,27,33]
-print('Problem 4:Test Output for matrix_vector_multi: ' + str(matrix_vector_multi(test_matrix_01,test_vector_02)))
-print('Should have been [21,27,33]')
 
 #Problem 05
 
-def matrix_matrix_multi(matrix_1: "list[list[float]]", matrix_2: "list[list[float]]"):
+def matrix_matrix_multi(matrix_1: list, matrix_2: list):
   """Multiplies a matrix by another matrix.
 
     We will create an empty result list and store the sums of the columns 
@@ -209,15 +145,8 @@ def matrix_matrix_multi(matrix_1: "list[list[float]]", matrix_2: "list[list[floa
        The multiplication of the input matrices stored as a new 
        matrix as result. 
     """
-  result: "list[list[float]]" = []
+  result = []
   for index in range(len(matrix_1)):
     result.append(matrix_vector_multi(matrix_1, matrix_2[index]))
   return result
 
-# matrix_matrix_multi(test_matrix_01,test_matrix_02) should output [[48,63,78],[115,143,171],[295,368,441]]
-print('Problem 5:Test Output for matrix_matrix_multi: ' + str(matrix_matrix_multi(test_matrix_01,test_matrix_02)))
-print('Should have been [[48,63,78],[115,143,171],[295,368,441]]')
-
-# matrix_matrix_multi(test_matrix_01,test_matrix_03) should output [[12,15,18],[24,30,36],[36,45,54]]
-print('Problem 5:Test Output for matrix_matrix_multi: ' + str(matrix_matrix_multi(test_matrix_01,test_matrix_03)))
-print('Should have been [[12,15,18],[24,30,36],[36,45,54]]')
