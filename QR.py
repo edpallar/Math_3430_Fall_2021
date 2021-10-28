359ff4472ae296acc2f5fd031a01815556d93b92
edpallar <90791746+edpallar@users.noreply.github.com>
2021-10-28 13:36:59 -0500

    Working program, wrong outputs

--- QR.py
+++ QR.py
@@ -3,7 +3,7 @@ import LA
 
 
 A = [[1,0,1],[2,1,0]]
-def unstable_gram_schmidt(A: list) -> list: 
+def unstable_gram_schmidt(A ) : 
     """Algorithm for an unstable gram-schmidt for reduced QR factorization
 
     How? 
@@ -17,7 +17,7 @@ def unstable_gram_schmidt(A: list) -> list:
     
 
 
-def gram_schmidt(matrix: list) -> list: 
+def gram_schmidt(matrix):
     """Algorithm for an unstable gram-schmidt for reduced QR factorization
     
     How? 
@@ -30,19 +30,31 @@ def gram_schmidt(matrix: list) -> list:
         A list of two matrices, Q and R. 
     """
     # make copy of A for Q where Q is A*0 
-    Q: list = []
+    Q = []
     for element in A:
       Q.append(element)
     Q = LA.matrix_scalar_multi(Q,0)
     # make V 
-    V: list = []
+    V = []
     for element in A:
       V.append(element)
-    # make R 
-    for element in A:
-      R: list = [0 for element in A]
-    
 
-    return(Q,R)
+    # make R
+    R = []
+    for element in A:
+      R.append([0 for index in range(len(A))])
+    
+    
+    for index in range(len(A)):
+      R[index][index]= LA.p_norm(V[index],2,False)
+      
+      Q[index] = LA.vector_scalar_multi(V[index], 1/R[index][index])
+      
+      
+      for inner_index in range(1,len(A)):
+        R[inner_index][index] = LA.inner_product(Q[index],V[inner_index])
+        V[inner_index] = LA.add_vectors(V[inner_index],LA.vector_scalar_multi(Q[index],-R[index][index]))
+
+    return(Q,R,V)
 
 print(gram_schmidt(A))
