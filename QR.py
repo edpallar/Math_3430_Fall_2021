f0416629a643470ca4a0cf168fda61eaa71e32ed
edpallar <90791746+edpallar@users.noreply.github.com>
2021-11-12 12:37:20 -0600

    HW 07 conjugate transpose problems

--- QR.py
+++ QR.py
@@ -1,7 +1,7 @@
 import LA
     
 
-def gram_schmidt(matrix:list) -> list:
+def gram_schmidt(matrix):
     """Algorithm for an unstable gram-schmidt for reduced QR factorization
     
     This program starts by setting up the three lists where we will store values. Q and R are the matrices we want to end up with, and V is just a copy of A that we want to manipulate and edit as we go through the code. The for loop with index and inner_index work as two different for loops to assess different elements of the input matrix. The inner_index will begin working on the next element of A to store in V, which is needed to compute the values of Q and R. We find the 2-norm of V to store in R, and then use that value to normalize V and store in Q. The code then uses the inner product of Q and V to find the next value of R. It then uses that new R value to update the next column of V by the equation V = V -Q*R for each index. 
@@ -13,17 +13,17 @@ def gram_schmidt(matrix:list) -> list:
     Returns: 
         A list of two matrices, Q and R. 
     """
-    Q: list = []
+    Q = []
     for element in matrix:
       Q.append(element)
     Q = LA.matrix_scalar_multi(Q,0)
     # make V 
-    V: list = []
+    V = []
     for element in matrix:
       V.append(element)
 
     # make R
-    R: list = []
+    R = []
     for element in matrix:
       R.append([0 for index in range(len(matrix))])
     
@@ -39,7 +39,7 @@ def gram_schmidt(matrix:list) -> list:
 
     return[Q,R]
 
-def orthonormal(matrix: list) -> list: 
+def orthonormal(matrix):
   """Finds the orthonormal of a set of vectors (a matrix). 
 
   Since Q is a set of orthonormal vectors in the gram_schimdt program, we can just return Q by returning the 0th element (first one) of the gram_schmidt program. 
@@ -49,7 +49,78 @@ def orthonormal(matrix: list) -> list:
   Returns: 
     Q: a matrix of orthonormal vectors from the input vectors 
   """
-  result: list = gram_schmidt(matrix)[0]
+  result = gram_schmidt(matrix)[0]
+  return result 
+
+A = [[1,0,1],[2,1,0]]
+matrix_a = [[2, -2j, 3-3j],[4, 4j, 4-4j]]
+
+def conjugate_transpose_1(vector):
+  """
+  
+  """
+  result = []
+  for index in range(len(vector)):
+    result.append(LA.conjugate(vector[index]))
+  return result
+
+def conjugate_transpose(matrix):
+  """
+  
+  """
+  #conjugate entire matrix first 
+  result = []
+  for index in range(len(matrix)):
+    result.append(conjugate_transpose_1(matrix[index]))
+  #now try to convert mxn to nxm matrix by making the columns the rows
+  joe = []
+
+  for row in range(len(matrix[0])):
+    for index in range(len(matrix)):
+      joe.append(result[index][row])
+  return joe 
+
+
+"""print(conjugate_transpose(matrix_a))"""
+
+def basis_vec_builder(int):
+  """
+  
+  """
+  result = [1]
+  for index in range(1,int):
+    result.append(0)
+  return result 
+
+def identity_builder(size):
+  """
+  
+  """
+  result = []
+  for r in range(0,size):
+    result.append([0 for r in range(0,size)])
+
+  for index in range(len(result)):
+    result[index][index] = 1
   return result 
 
 
+
+def householder(matrix):
+  """
+  
+  """
+  R = []
+  for element in matrix:
+    R.append(element) 
+  Q_list = []
+  Q = []
+  for index in range(len(matrix)-1):
+    #build Q here 
+    #append Q to Q_list
+    R = LA.matrix_matrix_multi(Q[index],R)
+  Q = conjugate_transpose(Q[1])
+  for index in range(len(Q_list)):
+    Q = LA.matrix_matrix_multi(Q,conjugate_transpose(Q_list[index]))
+  return [Q,R]
+  
