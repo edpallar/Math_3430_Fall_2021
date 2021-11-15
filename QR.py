0d605b3d74424f912045d48c9ef50952fb7264a9
edpallar <90791746+edpallar@users.noreply.github.com>
2021-11-15 11:19:40 -0600

    up to F_builder

--- QR.py
+++ QR.py
@@ -1,5 +1,5 @@
 import LA
-    
+
 
 def gram_schmidt(matrix):
     """Algorithm for an unstable gram-schmidt for reduced QR factorization
@@ -52,40 +52,73 @@ def orthonormal(matrix):
   result = gram_schmidt(matrix)[0]
   return result 
 
-A = [[1,0,1],[2,1,0]]
-matrix_a = [[2, -2j, 3-3j],[4, 4j, 4-4j]]
+
 
 def conjugate_transpose_1(vector):
-  """
+  """ Finds the conjugate of a vector 
   
+Runs through each element of the vector and calculates conjugate at each step. 
+
+  Args:
+      vector: A list  
+
+  Returns:
+      vector: A list of conjugate outputs from the vector input 
   """
   result = []
   for index in range(len(vector)):
     result.append(LA.conjugate(vector[index]))
   return result
 
-def conjugate_transpose(matrix):
-  """
+def transpose(matrix):
+  """Finds the transpose of a given matrix 
   
+By fixing the for loop across the first column, index runs through the elements of the row and appends them to temp. Once it is done with a row, it appends temp to result, then
+resets temp to be empty. 
+
+  Args:
+      matrix: A list of lists 
+
+  Returns:
+      matrix: A list of lists that is the tranpose of the input. 
+  """
+  result = []
+  for index in range(len(matrix[0])):
+    temp = []
+    for element in matrix:
+      temp.append(element[index])
+    result.append(temp)
+  return result 
+
+def conjugate_transpose(matrix):
+  """Finds the conjugate transpose of aa matrix.
+  
+  Using the conjugate function to first find the conjugates of each element of the given matrix. Then, use the transpose function to tranpose the matrix. 
+
+  Args:
+      matrix: A list of lists 
+
+  Returns:
+      matrix: A list of lists that is now the conjugate tranpose of the input.  
   """
   #conjugate entire matrix first 
   result = []
   for index in range(len(matrix)):
     result.append(conjugate_transpose_1(matrix[index]))
-  #now try to convert mxn to nxm matrix by making the columns the rows
-  joe = []
+  #tranpose matrix 
+  result = transpose(result)
+  return result 
 
-  for row in range(len(matrix[0])):
-    for index in range(len(matrix)):
-      joe.append(result[index][row])
-  return joe 
-
-
-"""print(conjugate_transpose(matrix_a))"""
 
 def basis_vec_builder(int):
-  """
+  """Gives a basis vector of a specified size 
   
+  Function creates a basis vector where the first element is 1 and the int input represents the number of rows and the number of 0's being added (int-1)
+  Args:
+      int: represents the number of rows in the vector 
+
+  Returns:
+      vector: The basis vector 
   """
   result = [1]
   for index in range(1,int):
@@ -93,8 +126,15 @@ def basis_vec_builder(int):
   return result 
 
 def identity_builder(size):
-  """
+  """Creates a square indentity matrix of a certain input size. 
   
+  The function sets up the size first by placing 0 in every spot. Then goes through the diagonal and replaces the 0 with a 1. 
+
+  Args:
+      size: an interger specificying the size of the matrix output
+
+  Returns:
+      matrix: Outputs a square identity matrix where the diagnol is made of 1's. 
   """
   result = []
   for r in range(0,size):
@@ -104,11 +144,92 @@ def identity_builder(size):
     result[index][index] = 1
   return result 
 
+def diag_vec(matrix):
+  """Returns the vector below the diagonal of the input matrix. 
 
+  It runs two for loops to index through each column and starts at the diagonal and adds the elements below that element. Runs through until i gets to the last element of the diagonal. 
+
+  Args:
+      matrix: a list of columns 
+
+  Returns:
+      matrix: A list of diag vecs according to their position in the orignal matrix
+  """
+  diag = []
+  temp = []
+  counter = 0
+
+  for i in matrix:
+    for j in i[counter:]:
+      temp.append(j);
+    diag.append(temp.copy())
+    counter += 1
+    temp.clear()
+  
+  return diag
+
+  
+def v_builder(matrix, column_number):
+  """Give the vector v needed for building F_builder
+  
+  Adds the two vectors where the first is the multiplication of the 2-norm of the retreived diag_vec and the basis vector. The second vector is just the retrieved diag_vec. 
+  
+
+  Args:
+      matrix: A matrix of lists of column vectors 
+      column_number: an integer that specifies which column of the matrix we are retrieving 
+
+  Returns:
+      resul: a vector that will be used in the F_builder function. 
+      
+  """
+  #v=||diag_vec||*e + diag_vec  
+  vec = diag_vec(matrix)[column_number]
+  print(vec)
+  v = LA.p_norm(vec)
+  print(v)
+  result = LA.add_vectors(LA.vector_scalar_multi(basis_vec_builder(len(vec)),v), vec)
+  return result 
+
+A = [[1,0,1],[2,1,0]]
+print(v_builder(A,0))
+
+  
+
+
+def F_builder(vector):
+  """This function returns the matrix F needed to build Q. 
+  
+  By using the v_builder function to access the necessary v for each iteration of F, we use the following equation to find F. 
+  F (for every column of A denoted by k)= I - 2* ((v*conjugate_tranpose(v))/(inner_product(v)))
+
+  Args:
+      vector: It will take in v from v_builder, which accesses our necessary input matrix. 
+
+  Returns:
+      matrix: Returns a matrix of the built F from the input vector v.
+  """
+  iden: list = identity_builder(len(vector))
+  print(iden,"iden")
+  two: float = 2/(LA.inner_product(vector,vector))
+  print(two,"two")
+  cv:list = LA. matrix_matrix_multi([vector],[conjugate_transpose([vector])])
+  print(cv,"cv")
+  result = LA.add_matrices(iden,-(LA.vector_scalar_multi(cv,two)))
+  print(result)
+  return result 
+  # F (for every column of A denoted by k)= I - 2* ((v*conjugate_tranpose(v))/(inner_product(v)))
 
 def householder(matrix):
-  """
+  """Gives the householder full QR factorization of a given matrix. 
   
+  
+
+  Args:
+      matrix ([type]): [description]
+
+  Returns:
+      [type]: [description]
   """
   R = []
   for element in matrix:
